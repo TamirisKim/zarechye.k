0f27463083edab6e7d95fac6b7cf9d4b49422341
TamirisKim <kimbaevat@gmail.com>
2022-09-16 18:28:08 +0600

    проба адаптив header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,19 @@
 console.log('file 1');
+
+let menuBtn = document.querySelector("[data-nav-toggler]");
+let navbar = document.querySelector("[data-navbar]");
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("close-btn");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menuBtn.classList.remove("close-btn");
+  navbar.classList.remove("active");
+};
+
+
+
+
+
